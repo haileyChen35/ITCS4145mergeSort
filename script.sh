f842c99fd7d81887ecddbd82173726ad19f6c254
haileyChen35 <hchen35@charlotte.edu>
2025-02-03 21:46:49 -0500

    first commit

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=myjob
+#SBATCH --partition=Centaurus
+#SBATCH --time=00:01:00
+
+./program 10
+./program 100
+./program 1000
+./program 10000
+./program 100000
+./program 1000000
+./program 10000000
+./program 100000000
+./program 1000000000
